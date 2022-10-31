af20b7b10c2cb19fa6126ce3d25741249aeb7f41
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-10-31 17:09:24 -0400

    landing page paragraph done

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -2,6 +2,8 @@ import {Link} from 'react-router-dom'
 
 const landing =()=>{
   return <div className="landing">
+    <h3>Welcome to Waterpark-Bites!</h3>
+    <p>Waterpark-Bites is your one-stop shop concession stand for all waterpark food. Take a break from your fun-filled day to enjoy some grub!</p>
   <Link to="/">Home</Link>
   </div>
 }
