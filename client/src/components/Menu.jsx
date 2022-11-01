5bb41d8aa0dad770678d8c1444a02dd9579c2daa
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-11-01 11:25:45 -0400

    menu page w/ links

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,34 +1,14 @@
-import Drinks from './Drinks'
-import Food from './Food'
-import Desserts from './Desserts'
-import {useState, useEffect} from 'react'
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Menu =()=>{
-  let navigate= useNavigate()
-
-  // const [menuState, setMenuState]=useState()
-
-  // const apiCall= async ()=>{
-  //   const response = await axios.get('http://localhost:3001/menu')
-  //   setMenuState(response.data)
-  // }
-
-  // useEffect (()=>{
-  //   apiCall()
-  // }, [])
 
   return(
     <div>
-      
-      <Drinks/>
-      <Food/>
-      <Desserts/>
+      <h2 className="drinksMain"> <Link to ="/drink">Drinks</Link></h2>
+      <h2 className="foodMain"> <Link to="/food">Food </Link></h2>
+      <h2 className="dessertsMain"> <Link to="/dessert">Desserts </Link></h2>
     </div>
   )
 }
 
 export default Menu
-
-//render food, drinks, desserts
