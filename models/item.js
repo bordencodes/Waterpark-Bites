0be723e95a262b57b534cf107ea17322110eda92
Alaysia Veal <alaysiaveal@gmail.com>
2022-10-31 21:10:20 -0400

    changes item type

--- models/item.js
+++ models/item.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const Item = new Schema(
   {
-    title: { type: String, required: true },
+    item: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
     cost: { type: Number, required: true }
