7b571ad59c444e6e33d325e8bc0948311b90c1df
Alaysia Veal <alaysiaveal@gmail.com>
2022-11-01 10:41:10 -0400

    added controllers and routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
 const cors = require('cors')
+const controllers = require('.//controllers')
 
 const PORT = process.env.PORT || 3001
 
@@ -13,6 +14,11 @@ app.use(cors())
 app.use('/api', routes)
 
 app.get('/hi', (req, res) => res.send('hello'))
+app.get('/food', controllers.findFood)
+app.get('/drink', controllers.findDrink)
+app.get('/dessert', controllers.findDessert)
+app.delete('/menu/:id', controllers.deleteItems)
+app.get('/allItems', controllers.findAllItems)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
