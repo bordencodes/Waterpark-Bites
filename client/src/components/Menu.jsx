e87d98c19c8664a578a9eb9cf81b30ef3c8f8ef1
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-11-01 22:21:00 -0400

    CSS grid worked on

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -4,6 +4,11 @@ const Menu =()=>{
 
   return(
     <div className='mainMenu'>
+        <img
+        className="waterpark-gif"
+        src="https://media.giphy.com/media/bsfUpZfjw8pBr2QKUg/giphy.gif"
+        alt="burger gif"
+      />
       <h2 className="menuList"> <Link to ="/drink">Drinks</Link></h2>
       <h2 className="menuList"> <Link to="/food">Food </Link></h2>
       <h2 className="menuList"> <Link to="/dessert">Desserts </Link></h2>
