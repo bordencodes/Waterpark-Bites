4cb710382c01def3e370193b5fa8ba6303453ebc
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-11-01 21:48:47 -0400

    gify added

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -4,10 +4,17 @@ const Landing = () => {
   return (
     <div className="landing">
       <h3 className="landing-title">Welcome to Waterpark-Bites!</h3>
+      <img
+        className="waterpark-gif"
+        src="https://media.giphy.com/media/xT39CUZqBfDaovFxMQ/giphy.gif"
+        alt="waterpark gif"
+      />
       <p className="landing-text">
         Take a break from your fun-filled day to enjoy some grub!
       </p>
-      <Link to="/menu">Menu</Link>
+      <span className="landing-menu">
+        <Link to="/menu">Menu</Link>
+      </span>
     </div>
   )
 }
